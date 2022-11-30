7b079f96011393800a4e696b7cee38f2fdd0feeb
IskanderSabirov <sabiroviskander@yandex.ru>
2022-11-30 21:53:20 +0300

    first version 2.0

--- lab-04_intrusive-list/src/main.c
+++ lab-04_intrusive-list/src/main.c
@@ -11,17 +11,17 @@ typedef struct point {
     struct intrusive_node node;
 } point;
 
-void add_point(intrusive_list *my_list, int x, int y) {
+void add_point(intrusive_list *list, int x, int y) {
     point *p = (point *) malloc(sizeof(point));
     p->x = x;
     p->y = y;
-    add_node(my_list, &(p->node));
+    add_node(list, &(p->node));
 }
 
 
-void remove_point(intrusive_list *my_list, int x, int y) {
+void remove_point(intrusive_list *list, int x, int y) {
 
-    intrusive_node *head = my_list->head;
+    intrusive_node *head = list->head;
 
 
     while (head->next != NULL) {
@@ -41,22 +41,25 @@ void remove_point(intrusive_list *my_list, int x, int y) {
 }
 
 
-void show_all_points(intrusive_list *my_list) {
-    intrusive_node *base = my_list->head;
-    while (base->next != NULL) {
+void show_all_points(intrusive_list *list) {
+    intrusive_node *base = list->head;
+    if (get_length(list) == 0) printf("\n");
+    else {
+        while (base->next != NULL) {
 
-        base = base->next;
-        point *print_point = container_of(base, point, node);
+            base = base->next;
+            point *print_point = container_of(base, point, node);
 
-        if (base->next != NULL) {
-            printf("(%d %d) ", print_point->x, print_point->y);
-        } else printf("(%d %d)\n", print_point->x, print_point->y);
+            if (base->next != NULL) {
+                printf("(%d %d) ", print_point->x, print_point->y);
+            } else printf("(%d %d)\n", print_point->x, print_point->y);
+        }
     }
 }
 
 
-void remove_all_points(intrusive_list *my_list) {
-    intrusive_node *base = my_list->head;
+void remove_all_points(intrusive_list *list) {
+    intrusive_node *base = list->head;
 
     while (base->next != NULL) {
 
@@ -69,8 +72,8 @@ void remove_all_points(intrusive_list *my_list) {
 }
 
 int main() {
-    intrusive_list my_list;
-    init(&my_list);
+    intrusive_list list;
+    init(&list);
 
     char arguments[239];
     int x, y;
@@ -82,24 +85,24 @@ int main() {
         if (strcmp(arguments, "add") == 0) {
 
             scanf("%d %d", &x, &y);
-            add_point(&my_list, x, y);
+            add_point(&list, x, y);
 
         } else if (strcmp(arguments, "rm") == 0) {
 
             scanf("%d %d", &x, &y);
-            remove_point(&my_list, x, y);
+            remove_point(&list, x, y);
 
         } else if (strcmp(arguments, "len") == 0) {
 
-            printf("%d\n", get_length(&my_list));
+            printf("%d\n", get_length(&list));
 
         } else if (strcmp(arguments, "rma") == 0) {
 
-            remove_all_points(&my_list);
+            remove_all_points(&list);
 
         } else if (strcmp(arguments, "print") == 0) {
 
-            show_all_points(&my_list);
+            show_all_points(&list);
 
         } else if (strcmp(arguments, "exit") == 0) {
 
@@ -112,7 +115,7 @@ int main() {
         }
     }
 
-    remove_all_points(&my_list);
-    free(my_list.head);
+    remove_all_points(&list);
+    free(list.head);
     return 0;
 }
