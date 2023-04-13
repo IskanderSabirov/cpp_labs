1581d939b71bcfbe708dd01354d7b5c8958b5a5b
IskanderSabirov <sabiroviskander@yandex.ru>
2023-04-13 19:38:55 +0300

    2/4

--- lab-13_array/include/my_array.h
+++ lab-13_array/include/my_array.h
@@ -23,6 +23,12 @@ namespace lab_13 {
                 data_[i] = t.data_[i];
         }
 
+        const T &at(std::size_t index) const {
+            if (index >= N)
+                throw std::out_of_range("Invalid index ");
+            return data_[index];
+        }
+
         T &at(std::size_t index) {
             if (index >= N)
                 throw std::out_of_range("Invalid index ");
@@ -38,11 +44,11 @@ namespace lab_13 {
         }
 
 
-         bool empty() const {
+        bool empty() const {
             return size_ == 0;
         }
 
-         std::size_t size() const {
+        std::size_t size() const {
             return size_;
         }
 
@@ -59,79 +65,78 @@ namespace lab_13 {
 
         std::size_t size_ = N;
 
-        uint8_t data_[N / sizeof(uint8_t) + (N % sizeof(uint8_t) == 0 ? 0 : 1)]{};
+        uint8_t data_[N / sizeof(uint8_t) + (N % sizeof(uint8_t) == 0 ? 0 : 1)] = {};
 
         std::size_t block_size_ = sizeof(uint8_t);
 
         class bool_manip {
         private:
-            std::size_t step_;
-            std::size_t &byte_;
+            uint8_t step_;
+            uint8_t &byte_;
         public:
+            bool_manip(uint8_t &byte, uint8_t step) : step_(step), byte_(byte) {}
 
-            bool_manip(uint8_t &byte, uint8_t step) {
-                step_ = step;
-                byte_ = byte;
-            }
-
-            bool_manip &operator=(const bool &val) {
+            bool_manip &operator=(bool val) {
                 if (val)
-                    byte_ |= (1 >> step_);
+                    byte_ |= (1 << step_);
                 else
-                    byte_ &= ~(1 >> step_);
+                    byte_ &= ~(1 << step_);
                 return *this;
             }
 
             bool_manip &operator=(const bool_manip &t) {
-                this->operator=(t.value());
+                byte_ = t.byte_;
+                step_ = t.step_;
                 return *this;
             }
 
-            bool value() {
+            operator bool() const {
                 return (byte_ >> step_) & 1;
             }
 
         };
 
     public:
+
         my_array() = default;
 
-         bool at(std::size_t index) const {
+        bool at(std::size_t index) const {
             if (index >= N)
                 throw std::out_of_range("Invalid index ");
-            return bool_manip(index / block_size_, index % block_size_).value();
+            return (*this)[index];
         }
 
         bool_manip at(std::size_t index) {
             if (index >= N)
                 throw std::out_of_range("Invalid index ");
-            return bool_manip(index / block_size_, index % block_size_);
+            return (*this)[index];
         }
 
 
         bool operator[](std::size_t index) const {
-            return bool_manip(index / block_size_, index % block_size_).value();
+            return data_[index / block_size_] >> index % block_size_ & 1;
         }
 
         bool_manip operator[](std::size_t index) {
-            return bool_manip(index / block_size_, index % block_size_);
+            return bool_manip(data_[index / block_size_], index % block_size_);
         }
 
-
-         bool empty() const {
+        bool empty() const {
             return size_ == 0;
         }
 
-         std::size_t size() const {
+        std::size_t size() const {
             return size_;
         }
 
         void fill(bool val) {
+            uint8_t t = (val ? -1 : 0);
             for (size_t i = 0; i < N; ++i)
-                data_[i] = val;
+                data_[i] = t;
         }
 
     };
+
 
 }  // namespace lab_13
 
