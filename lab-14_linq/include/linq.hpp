57e12c01f42daea928b8bad5fa60a39b98840ca9
IskanderSabirov <sabiroviskander@yandex.ru>
2023-05-14 13:01:03 +0300

    select 2.0

--- lab-14_linq/include/linq.hpp
+++ lab-14_linq/include/linq.hpp
@@ -146,31 +146,31 @@ namespace linq {
         template<typename T, typename U, typename F>
         class select_enumerator : public enumerator<T> {
         public:
-            select_enumerator(enumerator<U> &parent, F func) : parent_(parent), func_(std::move(func)) {
-                cur_ = func_(*parent);
+            select_enumerator(enumerator<U> &parent, F _func) : parent(parent), func(std::move(_func)) {
+                cache = func(*parent);
             }
 
-            select_enumerator(select_enumerator &&) noexcept = default;
+            select_enumerator(select_enumerator &&)  noexcept = default;
 
             virtual explicit operator bool() const {
-                return parent_.operator bool();
+                return (bool) parent;
             }
 
             virtual enumerator<T> &operator++() {
-                ++parent_;
-                if (parent_.operator bool())
-                    cur_ = func_(*parent_);
+                ++parent;
+                if ((bool) parent)
+                    cache = func(*parent);
                 return *this;
             }
 
             virtual const T &operator*() {
-                return cur_;
+                return cache;
             }
 
         private:
-            enumerator<U> &parent_;
-            F func_;
-            T cur_;
+            enumerator<U> &parent;
+            F func;
+            T cache;
         };
 
         template<typename T, typename F>
